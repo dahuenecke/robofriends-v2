879da1bd5fb0f64e8daba90018296b0956b381d6
danhuenecke <dan.huenecke@channelhealth.co>
2018-09-13 11:39:40 -0500

    Initial Commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Dan Huenecke',
+        username: 'Danimal',
+        email: 'dan@robofriends.com'
+    },
+    {
+        id: 2,
+        name: 'Steve Smith',
+        username: 'Smitty',
+        email: 'steve@robofriends.com'
+    },
+    {
+        id: 3,
+        name: 'Ervin Howell',
+        username: 'Erv',
+        email: 'erv@robofriends.com'
+    },
+    {
+        id: 4,
+        name: 'George Cashew',
+        username: 'Cash Money',
+        email: 'george@robofriends.com'
+    },
+    {
+        id: 5,
+        name: 'Rick Stevens',
+        username: 'Rick Roll',
+        email: 'rick@robofriends.com'
+    },
+    {
+        id: 6,
+        name: 'Lynsee Gonzales',
+        username: 'Lyns',
+        email: 'lynsee@robofriends.com'
+    },
+    {
+        id: 7,
+        name: 'Shari',
+        username: 'Shari Baby',
+        email: 'shari@robofriends.com'
+    },
+    {
+        id: 8,
+        name: 'Randy Johnson',
+        username: 'Big Unit',
+        email: 'randy@robofriends.com'
+    },
+    {
+        id: 9,
+        name: 'Hank Huenecke',
+        username: 'Hankey Pankey',
+        email: 'hank@robofriends.com'
+    },
+    {
+        id: 10,
+        name: 'Joe Diffie',
+        username: 'Diffie',
+        email: 'joe@robofriends.com'
+    },
+]
